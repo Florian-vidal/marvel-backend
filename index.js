345b03fd712a995fe2509388c4cb76838dcd8bc5
ezekiel <florian.vidal@lapiscine.pro>
2025-09-06 15:43:23 +0200

    first backend on github

---
+++ index.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  try {
+    return res.status(200).json("Bienvenue sur le serveur marvel 🦸‍♂️🦸🏽‍♀️");
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/characters", async (req, res) => {
+  try {
+    const limit = req.query.limit || 100;
+    const skip = req.query.skip || 0;
+
+    let queries = `&limit=${limit}&skip=${skip}`;
+
+    if (req.query.name) {
+      queries = queries + "&name=" + req.query.name;
+    }
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}${queries}`
+    );
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const limit = req.query.limit || 100;
+    const skip = req.query.skip || 0;
+
+    let queries = `&limit=${limit}&skip=${skip}`;
+
+    if (req.query.title) {
+      queries = queries + "&title=" + req.query.title;
+    }
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}${queries}`
+    );
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/comics/:characterId", async (req, res) => {
+  try {
+    // utiliser axios pour envoyer une requête à l'API :
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started 🦸‍♂️🦸🏽‍♀️");
+});
